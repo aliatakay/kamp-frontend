46253a1e5967f6d619caee889bedd16be612d7dc
Ali Atakay <aliatakay@gmail.com>
2021-03-14 19:23:08 +0300

    Added HttpClient Module for Product

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -8,7 +10,26 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  constructor() {}
 
-  ngOnInit(): void {}
+  apiUrl = 'https://localhost:44368/api/products/getall';
+
+  productResponseModel: ProductResponseModel = {
+    data: this.products,
+    message: '',
+    success: true,
+  };
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data;
+      });
+  }
 }
